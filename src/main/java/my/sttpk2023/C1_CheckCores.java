af6521c55a0e06c22a36d20ac2ee5b2565339d62
zhamri <zhamri@gmail.com>
2026-05-18 11:08:28 +0800

    thread experiment

--- src/main/java/my/sttpk2023/C1_CheckCores.java
+++ src/main/java/my/sttpk2023/C1_CheckCores.java
@@ -6,3 +6,7 @@ public class C1_CheckCores {
         System.out.println(cores);
     }
 }
+
+/*
+14
+ */
--- src/main/java/my/sttpk2023/E6_ExecutorServiceExperiment.java
+++ src/main/java/my/sttpk2023/E6_ExecutorServiceExperiment.java
@@ -23,12 +23,12 @@ public class E6_ExecutorServiceExperiment {
 
     public static void main(String[] args) throws InterruptedException {
 
-        int cores = 10;
+        int cores = 14;
         long startTime = System.currentTimeMillis();
 
         ExecutorService executor = Executors.newFixedThreadPool(cores);
 
-        for (int i = 0; i < cores; i++) {
+        for (int i = 0; i < 30; i++) {
             executor.execute(new PoolTask());
         }
 
@@ -41,3 +41,21 @@ public class E6_ExecutorServiceExperiment {
         System.out.println("Execution Time: " + (endTime - startTime) + " ms");
     }
 }
+
+/*
+pool-1-thread-4 completed.
+pool-1-thread-12 completed.
+pool-1-thread-6 completed.
+pool-1-thread-7 completed.
+pool-1-thread-10 completed.
+pool-1-thread-13 completed.
+pool-1-thread-8 completed.
+pool-1-thread-5 completed.
+pool-1-thread-11 completed.
+pool-1-thread-9 completed.
+pool-1-thread-14 completed.
+pool-1-thread-3 completed.
+pool-1-thread-1 completed.
+pool-1-thread-2 completed.
+Execution Time: 125 ms
+ */
